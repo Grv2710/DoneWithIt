5f333a7e219168010d15a947346a47db0142e926
Grv2710 <grv.kumar2710@gmail.com>
2024-08-21 11:33:44 +0530

    updated ListItem componnet

--- app/components/ListItem.js
+++ app/components/ListItem.js
@@ -1,24 +1,39 @@
 import { View, Image, StyleSheet } from "react-native";
 import React from "react";
 import AppText from "./AppText";
+import colors from "../config/colors";
 
 const ListItem = ({ title, subTitle, image }) => {
   return (
-    <View>
-      <Image source={image} style={styles.image} />
-      <View>
-        <AppText>onixevo27@gmail.com</AppText>
-        <AppText>5 Listing</AppText>
+    <View style={styles.container}>
+      <Image source={{ uri: image }} style={styles.image} />
+      <View style={styles.description}>
+        <AppText style={styles.title}>{title}</AppText>
+        <AppText style={styles.subTitle}>{subTitle}</AppText>
       </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+  },
+  description: {
+    alignItems: "center",
+    marginLeft: 10,
+  },
   image: {
     width: 70,
     height: 70,
     borderRadius: 35,
+    backgroundColor: "red",
+  },
+  subTitle: {
+    color: colors.light,
+  },
+  title: {
+    fontWeight: "500",
   },
 });
 export default ListItem;
