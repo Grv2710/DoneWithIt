b07f4a996c6889dd69f403e851e2b4fa54090438
Grv2710 <grv.kumar2710@gmail.com>
2024-09-01 13:02:15 +0530

    pushing updated code implemented pull to refresh

--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -1,4 +1,4 @@
-import { View, FlatList, Text } from "react-native";
+import { View, FlatList, Text, StyleSheet } from "react-native";
 import React, { useState } from "react";
 import ListItem from "../components/ListItem";
 import Screen from "../components/Screen";
@@ -21,30 +21,41 @@ const initialMessages = [
 ];
 const MessagesScreen = () => {
   const [messages, setMessages] = useState(initialMessages);
+  const [refreshing, setRefreshing] = useState(false);
 
   const handleDeleteMsg = (message) => {
     setMessages(messages.filter((m) => m.id !== message.id));
   };
   return (
     <Screen>
-      <View>
-        <FlatList
-          data={messages}
-          keyExtractor={(message) => message.id.toString()}
-          renderItem={({ item }) => (
-            <ListItem
-              onPress={() => console.log("clicked")}
-              title={item.title}
-              subTitle={item.description}
-              image={item.image}
-              renderRightActions={() => (
-                <ListItemDeleteAction onPress={() => handleDeleteMsg(item)} />
-              )}
-            />
-          )}
-          ItemSeparatorComponent={ListItemSeparator}
-        />
-      </View>
+      <FlatList
+        data={messages}
+        keyExtractor={(message) => message.id.toString()}
+        renderItem={({ item }) => (
+          <ListItem
+            onPress={() => console.log("clicked")}
+            title={item.title}
+            subTitle={item.description}
+            image={item.image}
+            renderRightActions={() => (
+              <ListItemDeleteAction onPress={() => handleDeleteMsg(item)} />
+            )}
+          />
+        )}
+        ItemSeparatorComponent={ListItemSeparator}
+        refreshing={refreshing}
+        onRefresh={() => {
+          setMessages([
+            {
+              id: 2,
+              title: "T2",
+              description: "D2",
+              image:
+                "https://cdn-icons-png.flaticon.com/512/10226/10226381.png",
+            },
+          ]);
+        }}
+      />
     </Screen>
   );
 };
